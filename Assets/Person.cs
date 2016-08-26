42eaa56b9232c977f964909a4cb9a56af75458be
kaoshunter09 <myminecraftLP09@gmail.com>
2016-08-26 09:01:08 +0100

    I've made a robot that will chase the player.

---
+++ Assets/Person.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenuAttribute]
+public class Person : ScriptableObject {
+	public string weapons;
+	public int age;
+}
---
+++ Assets/YourComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YourComponent : MonoBehaviour {
+
+	void Start(){
+		Debug.Log ("Start");
+	}
+
+	void Update(){
+		Debug.LogFormat ("Time={0}", Time.time);
+	}
+}
+	
